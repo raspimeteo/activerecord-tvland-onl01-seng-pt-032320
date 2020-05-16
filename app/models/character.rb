6f973bebd68aa823c1a2fa239bf96a04e965c5e3
John Zonneveld <jjczon@gmail.com>
2020-05-16 18:26:05 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -4,7 +4,7 @@ class Character < ActiveRecord::Base
   
   def say_that_thing_you_say
     
-    puts "#{self.name} always says: #{self.catchphrase}""
+    puts "#{self.name} always says: #{self.catchphrase}"
   end
   
 end
