e195052be9fe9cb18bb0dfa8cdf307fc2792d1c4
John Zonneveld <jjczon@gmail.com>
2020-05-16 18:19:29 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -9,7 +9,7 @@ class Actor < ActiveRecord::Base
   
   def list_roles
     self.characters.map do |character|
-      ('#{character.name}',',', 'Show.find_by_id(character.show_id).name')
+      '#{character.name}',',', '#{Show.find_by_id(character.show_id).name}'
     end
   end
   
